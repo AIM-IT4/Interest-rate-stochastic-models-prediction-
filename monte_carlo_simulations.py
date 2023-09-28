1d8276b500841e13d9ad4f1812b2f8b6cdfa7d90
Amit Kumar Jha <77675138+AIM-IT4@users.noreply.github.com>
2023-09-28 19:06:09 +0530

    Update monte_carlo_simulations.py

--- monte_carlo_simulations.py
+++ monte_carlo_simulations.py
@@ -14,17 +14,87 @@ kappa = 0.5
 theta = 0.04
 sigma = 0.01
 
-# Monte Carlo simulation for Vasicek Model
-rates_vasicek = np.zeros((num_paths, num_steps))
-rates_vasicek[:, 0] = initial_rate
+# Adjusting the simulation setup for 1 year with monthly intervals
+num_steps = 12
+dt = 1/12  # monthly steps
+time = np.arange(0, 1, dt)
+
+# Vasicek Model Simulation
+rates_vasicek_1yr = np.zeros((num_paths, num_steps))
+rates_vasicek_1yr[:, 0] = initial_rate
 for i in range(1, num_steps):
     brownian = np.random.normal(0, np.sqrt(dt), num_paths)
-    rates_vasicek[:, i] = rates_vasicek[:, i-1] + kappa * (theta - rates_vasicek[:, i-1]) * dt + sigma * brownian
+    rates_vasicek_1yr[:, i] = rates_vasicek_1yr[:, i-1] + kappa * (theta - rates_vasicek_1yr[:, i-1]) * dt + sigma * brownian
 
-# ... (similar code for CIR, Hull-White, and BDT models)
+# CIR Model Simulation
+rates_cir_1yr = np.zeros((num_paths, num_steps))
+rates_cir_1yr[:, 0] = initial_rate
+for i in range(1, num_steps):
+    brownian = np.random.normal(0, np.sqrt(dt), num_paths)
+    rates_cir_1yr[:, i] = rates_cir_1yr[:, i-1] + kappa_cir * (theta_cir - rates_cir_1yr[:, i-1]) * dt + sigma_cir * np.sqrt(rates_cir_1yr[:, i-1]) * brownian
+rates_cir_1yr = np.maximum(rates_cir_1yr, 0)
 
-# Visualization (subplots for each model)
+# Hull-White Model Simulation
+rates_hw_1yr = np.zeros((num_paths, num_steps))
+rates_hw_1yr[:, 0] = initial_rate
+for i in range(1, num_steps):
+    brownian = np.random.normal(0, np.sqrt(dt), num_paths)
+    rates_hw_1yr[:, i] = rates_hw_1yr[:, i-1] + kappa_hw * (theta_hw - rates_hw_1yr[:, i-1]) * dt + sigma_hw * brownian
 
-# ... (subplot code)
+# Approximated BDT Model Simulation
+rates_bdt_1yr = np.zeros((num_paths, num_steps))
+rates_bdt_1yr[:, 0] = initial_rate
+for i in range(1, num_steps):
+    brownian = np.random.normal(0, np.sqrt(dt), num_paths)
+    rates_bdt_1yr[:, i] = rates_bdt_1yr[:, i-1] + kappa_bdt * (theta_bdt - rates_bdt_1yr[:, i-1]) * dt + sigma_bdt * brownian
 
+# Create subplots for all the models over 1 year
+fig, axs = plt.subplots(2, 2, figsize=(15, 10))
+
+# Plot Vasicek Model simulations
+for i in range(num_paths):
+    axs[0, 0].plot(time, rates_vasicek_1yr[i, :], lw=0.8)
+axs[0, 0].set_title('Vasicek Model')
+axs[0, 0].set_ylabel('Interest Rate')
+axs[0, 0].grid(True)
+
+# Plot CIR Model simulations
+for i in range(num_paths):
+    axs[0, 1].plot(time, rates_cir_1yr[i, :], lw=0.8)
+axs[0, 1].set_title('Cox-Ingersoll-Ross (CIR) Model')
+axs[0, 1].grid(True)
+
+# Plot Hull-White Model simulations
+for i in range(num_paths):
+    axs[1, 0].plot(time, rates_hw_1yr[i, :], lw=0.8)
+axs[1, 0].set_title('Hull-White Model')
+axs[1, 0].set_xlabel('Years')
+axs[1, 0].set_ylabel('Interest Rate')
+axs[1, 0].grid(True)
+
+# Plot BDT Model simulations
+for i in range(num_paths):
+    axs[1, 1].plot(time, rates_bdt_1yr[i, :], lw=0.8)
+axs[1, 1].set_title('Black-Derman-Toy (BDT) Model (Approximated)')
+axs[1, 1].set_xlabel('Years')
+axs[1, 1].grid(True)
+
+plt.tight_layout()
 plt.show()
+
+#Approx values 
+
+# Extract predicted rates at the end of the next month and compute mean and standard deviation
+mean_vasicek = np.mean(rates_vasicek_1yr[:, 1])
+std_vasicek = np.std(rates_vasicek_1yr[:, 1])
+
+mean_cir = np.mean(rates_cir_1yr[:, 1])
+std_cir = np.std(rates_cir_1yr[:, 1])
+
+mean_hw = np.mean(rates_hw_1yr[:, 1])
+std_hw = np.std(rates_hw_1yr[:, 1])
+
+mean_bdt = np.mean(rates_bdt_1yr[:, 1])
+std_bdt = np.std(rates_bdt_1yr[:, 1])
+
+mean_vasicek, std_vasicek, mean_cir, std_cir, mean_hw, std_hw, mean_bdt, std_bdt
